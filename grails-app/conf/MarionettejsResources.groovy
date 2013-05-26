a4734f23e08807af4a20175495faaf0880dc25b1
Roberto Guerra <uris77@gmail.com>
2013-05-26 10:48:44 -0600

    Removed explicit backbone and underscore depencency.

--- grails-app/conf/MarionettejsResources.groovy
+++ grails-app/conf/MarionettejsResources.groovy
@@ -1,7 +1,6 @@
 modules = {	
 
 	marionettejs {
-		dependsOn 'underscore', 'backbone'
 		resource url: [plugin: 'marionettejs', dir: 'js', file: 'backbone.babysitter.js']
 		resource url: [plugin: 'marionettejs', dir: 'js', file: 'backbone.wreqr.js']
 		resource url: [plugin: 'marionettejs', dir: 'js', file: 'backbone.marionette.js']
