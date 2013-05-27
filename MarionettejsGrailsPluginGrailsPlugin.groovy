843bcd9ed22e1ecffd7a8029b78c69281db640a7
Burt Beckwith <burt@burtbeckwith.com>
2013-05-27 05:52:52 -0400

    added issueManagement

--- MarionettejsGrailsPluginGrailsPlugin.groovy
+++ MarionettejsGrailsPluginGrailsPlugin.groovy
@@ -14,5 +14,6 @@ built on top of Backbonejs. For more information on marionettejs go to http://ma
   
     def license = "APACHE"
     def organization = [name: 'Spantree LLC', url: 'http://spantree.net']
+    def issueManagement = [system: "GitHub", url: "https://github.com/Spantree/marionettejs-grails-plugin/issues"]
     def scm = [ url: "https://github.com/Spantree/marionettejs-grails-plugin" ]
 }
